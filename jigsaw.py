b53427810d3b3f8d10203a093f91a0489198bbcb
柴士童 <chaihahaha@qq.com>
2021-05-26 13:39:02 +0800

    improve

--- jigsaw.py
+++ jigsaw.py
@@ -2,11 +2,22 @@ from ortools.linear_solver import pywraplp
 from utils import *
 from data import *
 import math as m
+from numpy import arange
+import time
 
 
 solver = pywraplp.Solver.CreateSolver('SCIP')
 h = len(I0)
 w = len(I0[0])
+di_step = 1
+dj_step = 1
+dθ_step = m.pi*0.5
+
+T = {}
+for di in range(-h//2, h//2, di_step):
+    for dj in range(-w//2, w//2, dj_step):
+        for dθ in arange(0.0, m.pi*2, dθ_step):
+            T[di, dj, dθ] = affine_transform_matrix(di, dj, dθ, h, w)
 
 var = []
 Ms = []
@@ -14,20 +25,20 @@ for k in range(len(Is)):
     I = Is[k]
     xTs = []
     xs = []
-    for di in range(-h//2,h//2):
-        for dj in range(-w//2,w//2):
-            for dθ in [0, m.pi*0.5, m.pi, m.pi*1.5]:
-                T = affine_transform_matrix(di, dj, dθ, h, w)
-                x = solver.IntVar(0.0, 1.0, str(k)+','+str(di)+','+str(dj)+','+str(int(dθ/(m.pi*0.5))))
+    for di in range(-h//2, h//2, di_step):
+        for dj in range(-w//2, w//2, dj_step):
+            for dθ in arange(0.0, m.pi*2, dθ_step):
+                x = solver.IntVar(0.0, 1.0, str(k)+','+str(di)+','+str(dj)+','+"{:.2f}".format(dθ))
                 xs.append(x)
                 var.append(x)
-                xT = mul(x, T)
+                xT = mul(x, T[di, dj, dθ])
                 xTs.append(xT)
     M = column(I)
     M = matmul(matsum(xTs), M)
     Ms.append(M)
 
     solver.Add(solver.Sum(xs) == 1)
+del T
 
 I_col = matsum(Ms)
 lessthan(I_col, 1, solver)
@@ -41,8 +52,8 @@ if status == pywraplp.Solver.OPTIMAL:
     print('Objective value =', solver.Objective().Value())
     for x in var:
         if x.solution_value() > 0:
-            I_i, di, dj, dθ = tuple(map(int, str(x).split(',')))
-            print("piece:",I_i, "Δx:", dj, "Δy:",di, "Δθ", dθ)
+            I_i, di, dj, dθ = str(x).split(',')
+            print("piece:",I_i, "\tΔx:", dj, "\tΔy:",di, "\tΔθ", dθ)
 
     
     for k in range(len(Is)):
